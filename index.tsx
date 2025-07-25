5e4f06e532d6f0c1cee968b5e31fa9ac11c99577
Pratik <prateekraiger123@gmail.com>
2025-07-25 17:53:40 +0530

    updated layout

--- index.tsx
+++ index.tsx
@@ -1,10 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./src/App";
+import "./src/index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
